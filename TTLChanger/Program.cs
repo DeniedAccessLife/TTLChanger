301640ef99a7d70da25e47abefd4f817bd013688
DeniedAccessLife <89941208+DeniedAccessLife@users.noreply.github.com>
2023-10-31 20:49:49 +0300

    Add files via upload

---
+++ TTLChanger/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Win32;
+
+class Program
+{
+    private static readonly int value = 65;
+
+    static void Main(string[] args)
+    {
+        try
+        {
+            using (RegistryKey regKeyIPv4 = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Services\Tcpip\Parameters", true))
+            {
+                if (regKeyIPv4 == null)
+                {
+                    Console.WriteLine("Registry key for IPv4 could not be found!");
+                    return;
+                }
+
+                regKeyIPv4.SetValue("DefaultTTL", value, RegistryValueKind.DWord);
+                Console.WriteLine($"TTL value for IPv4 changed to {value}.");
+            }
+
+            using (RegistryKey regKeyIPv6 = Registry.LocalMachine.OpenSubKey(@"SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters", true))
+            {
+                if (regKeyIPv6 == null)
+                {
+                    Console.WriteLine("Registry key for IPv6 could not be found!");
+                    return;
+                }
+
+                regKeyIPv6.SetValue("DefaultTTL", value, RegistryValueKind.DWord);
+                Console.WriteLine($"TTL value for IPv6 changed to {value}.");
+            }
+
+            Console.ReadKey();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
